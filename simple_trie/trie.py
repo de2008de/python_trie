717653962b5f66c4cd71091bfdd87335db8e0b0b
Yi Ching (David) Chou <mrycchou@gmail.com>
2022-08-31 18:25:00 -0700

    Refactor code
    Change root to be Node
    Improve code to make them shorter and simpler

--- simple_trie/trie.py
+++ simple_trie/trie.py
@@ -18,21 +18,20 @@ class Node:
 
 class Trie:
     def __init__(self):
-        self.root = dict()
+        self.root = Node('')
 
     def insert(self, s: str):
-        if s[0] not in self.root:
-            self.root[s[0]] = Node(s[0])
-        prev = self.root[s[0]]
+        if not self.root.has_next(s[0]):
+            self.root.append_char(s[0])
+        prev = self.root.next[s[0]]
         for i in range(1, len(s)):
             prev = prev.append_char(s[i])
         prev.is_end = True
 
     def search(self, s: str):
-        if s[0] not in self.root:
+        if not self.root.has_next(s[0]):
             return False
-        prev = self.root[s[0]]
-
+        prev = self.root.next[s[0]]
         for i in range(1, len(s)):
             prev = prev.has_next(s[i])
             if prev is False:
@@ -42,28 +41,18 @@ class Trie:
         return False
 
     def delete(self, s: str):
-        if s[0] not in self.root:
+        list_node = [self.root]
+        for i in range(0, len(s)):
+            list_node.append(list_node[-1].has_next(s[i]))
+            if list_node[-1] is False:
+                return
+        if not list_node[-1].is_end:
             return
-        prev = self.root[s[0]]
-        list_node = [prev]
-
-        for i in range(1, len(s)):
-            prev = prev.has_next(s[i])
-            list_node.append(prev)
-            if prev is False:
-                return
-            if i == len(s) - 1 and prev.is_end is not True:
-                return
-
-        while len(list_node) != 0:
-            last_node = list_node[-1]
-            last_node.is_end = False
-            if len(last_node.next) == 0:
-                if len(list_node) > 1:
-                    del list_node[-2].next[last_node.char]
-                    del list_node[-1]
-                else:
-                    del self.root[last_node.char]
-                    del list_node[-1]
+        else:
+            list_node[-1].is_end = False
+        while len(list_node) != 2:
+            if len(list_node[-1].next) == 0:
+                del list_node[-2].next[list_node[-1].char]
+                del list_node[-1]
             else:
                 break
